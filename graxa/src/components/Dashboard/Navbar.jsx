1116181ac6d698e80ba7aa832f44b3b969185b94
ErickHlee <erick.hlee@sptech.school>
2025-10-15 11:16:41 -0300

    chore: adicionando deps do API Calendário

--- graxa/src/components/Dashboard/Navbar.jsx
+++ graxa/src/components/Dashboard/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MdSpaceDashboard, MdVideoCall } from "react-icons/md";
-import { FaCalendarAlt } from "react-icons/fa";
-import { IoMdSettings } from "react-icons/io";
+import { MdSpaceDashboard, MdStadium } from "react-icons/md";
+import { FaCalendarAlt, FaUsers } from "react-icons/fa";
 
 export const Navbar = () => {
   return (
@@ -29,7 +28,7 @@ export const Navbar = () => {
             to=""
             className="w-12 h-12 rounded-full hover:bg-neutral-300/20 flex items-center justify-center text-lg text-neutral-100 hover:text-neutral-200 hover:scale-110 transition-all duration-300"
           >
-            <IoMdSettings />
+            <MdStadium />
           </Link>
         </li>
         <li>
@@ -37,7 +36,7 @@ export const Navbar = () => {
             to=""
             className="w-12 h-12 rounded-full hover:bg-neutral-300/20 flex items-center justify-center text-lg text-neutral-100 hover:text-neutral-200 hover:scale-110 transition-all duration-300"
           >
-            <MdVideoCall />
+            <FaUsers />
           </Link>
         </li>
       </ul>
