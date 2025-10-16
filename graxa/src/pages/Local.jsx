d784c26cb954fb948ee5cb9ae1999774a906d490
ErickHlee <erick.hlee@sptech.school>
2025-10-16 10:44:33 -0300

    feat: adicionando telas - grupo, local, shows

--- graxa/src/pages/Local.jsx
+++ graxa/src/pages/Local.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import { Navbar } from "../components/Dashboard/Navbar";
 import { RootLayout } from "../components/Dashboard/RootLayout";
 import BgImg from "/cadastro-bg2.png";
-import { Layout } from "../components/Dashboard/Layout";
+import { Navbar } from "../components/Dashboard/Navbar";
+import { SecondaryNav } from "../components/Dashboard/SecondaryNav";
+import { Calendar } from "../components/Dashboard/Calendar";
+import { Content } from "../components/Dashboard/Content";
 
 export const Local = () => {
   return (
-    <div>
-      <RootLayout>
-        {/* Background Image */}
-        <img
-          className="img w-full h-full object-cover object-center absolute top-0 left-0"
-          src={BgImg}
-          alt="Background Image"
-        />
+    <RootLayout>
+      {/* Background Image */}
+      <img
+        className="img w-full h-full object-cover object-center absolute top-0 left-0"
+        src={BgImg}
+        alt="Background Image"
+      />
 
-        {/* Bg Overlay */}
-        <div className="w-full h-full absolute top-0 left-0 bg-gradient to-tr from-neutral-950/40 to-neutral-950/40"></div>
+      {/* Bg Overlay */}
+      <div className="w-full h-full absolute top-0 left-0 bg-gradient to-tr from-neutral-950/40 to-neutral-950/40"></div>
 
-        {/* Layout Section */}
-        <div className="w-full flex items-center gap-7 flex-wrap z-50">
-          {/* Navbar */}
-          <Navbar />
+      {/* Layout Section */}
+      <div className="w-full flex items-center gap-7 flex-wrap z-50">
+        {/* Navbar */}
+        <Navbar />
 
-          <Layout>{/* Dashboard */}</Layout>
-        </div>
-      </RootLayout>
-    </div>
+        {/* Dashboard */}
+        <Content>
+          <SecondaryNav />
+
+          {/* CODAR A PARTIR DAQUI VVVVVVV */}
+          <div>codar a partir daqui</div>
+        </Content>
+      </div>
+    </RootLayout>
   );
 };
