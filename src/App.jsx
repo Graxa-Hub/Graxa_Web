005495f1b45450599e5b11e1e342b80d1737388a
ErickHlee <erick.hlee@sptech.school>
2025-11-28 03:05:04 -0300

    remove:arquivos testes

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import { Calendario } from "./pages/Calendario";
 import { RecuperarSenha } from "./pages/RecuperarSenha";
 import { CriarEvento } from "./pages/CriarEvento";
 import { CriarLogistica } from "./pages/CriarLogistica";
-import WeatherExample from "./components/WeatherExample";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -28,16 +27,15 @@ function App() {
       <Route path="/recuperar-senha" element={<RecuperarSenha />} />
       <Route path="/cadastro" element={<Cadastro />} />
       {/* Rotas protegidas */}
-      {/* <Route element={<ProtectedLayout />}> */}
-      <Route path="/calendario" element={<Calendario />} />
-      <Route path="/turne/:bandaId?" element={<Turne />} />
-      <Route path="/adicionando-usuario" element={<AdicionandoUsuarios />} />
-      <Route path="/artista" element={<ArtistaApp />} />
-      <Route path="/visao-evento" element={<VisaoEvento />} />
-      <Route path="/criar-evento" element={<CriarEvento />} />
-      <Route path="/criar-logistica" element={<CriarLogistica />} />
-      <Route path="/weather-example" element={<WeatherExample />} />
-      {/* </Route> */}
+      <Route element={<ProtectedLayout />}>
+        <Route path="/calendario" element={<Calendario />} />
+        <Route path="/turne/:bandaId?" element={<Turne />} />
+        <Route path="/adicionando-usuario" element={<AdicionandoUsuarios />} />
+        <Route path="/artista" element={<ArtistaApp />} />
+        <Route path="/visao-evento" element={<VisaoEvento />} />
+        <Route path="/criar-evento" element={<CriarEvento />} />
+        <Route path="/criar-logistica" element={<CriarLogistica />} />
+      </Route>
       {/* Rota não encontrada - redireciona para login */}
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
